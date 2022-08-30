ed150655c022463d4f56908b417669c8214be058
kengo nakajima <kengo.nakajima@gmail.com>
2022-08-30 17:25:47 +0900

    othercaster

--- server/sv.js
+++ server/sv.js
@@ -9,11 +9,31 @@ const WS=require("ws");
 
 const wsv = new WS.WebSocketServer({ port: 13478 });
 
+const g_clients=[];
+function othercast(sender,data) {
+  for(let i in g_clients) {
+    if(g_clients[i]!=sender) g_clients[i].send(data);
+  }
+}
 wsv.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  g_clients.push(ws);
+  console.log("connection: clients:",g_clients.length);
+  ws.on('close', ()=>{
+    const ind=g_clients.indexOf(ws);
+    if(ind>=0) {
+      g_clients.splice(ind,1);
+      console.log("removed. clients:",g_clients.length);
+    }
   });
-
+  ws.on('message',(data)=>{
+    const s=data.toString();
+    const tks=s.split(" ");
+    console.log('received: %s',tks);
+    const cmd=tks[0];
+    if(cmd=="broadcast") {
+      othercast(ws,data);
+    }    
+  });
   ws.send('something');
 });
 
