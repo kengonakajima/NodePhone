4161f40fead237e653b0afe058bf07a3b60bcb00
kengo nakajima <kengo.nakajima@gmail.com>
2022-09-02 12:49:49 +0900

    state

--- server/cl.js
+++ server/cl.js
@@ -1,6 +1,11 @@
 const WS=require("ws");
 
-const cl = new WS.WebSocket('ws://localhost:13478/');
+const dest_host=process.argv[2];
+if(!dest_host) {
+  console.log("need ip address of server");
+  process.exit(1);
+}
+const cl = new WS.WebSocket(`ws://${dest_host}:13478/`);
 
 cl.on('open', function open() {
   cl.send('something');
@@ -13,6 +18,6 @@ cl.on('message', function message(data) {
 let g_counter=0;
 
 setInterval(function(){
-  cl.send("othercast "+process.pid+" "+g_counter);
+  if(cl.readyState==1) cl.send("othercast "+process.pid+" "+g_counter);
   g_counter++;
 },25); 
