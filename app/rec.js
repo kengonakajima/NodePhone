abc3a23706184abb32654a88ebc5ddf8f66c1301
kengo nakajima <kengo.nakajima@gmail.com>
2022-10-08 08:08:39 +0900

    const

--- app/rec.js
+++ app/rec.js
@@ -9,9 +9,9 @@ recorder
     .stream()
     .on('error', console.error) // エラーが起きたときにログを出力する
     .on('data', function(data) { // マイクからデータを受信する無名コールバック関数
-        var vol = Math.abs(data.readInt16LE());  // 配列の先頭のサンプリングデータをひとつ読み込み、音量を得る
-        var ntimes = vol / 512; // 音量が0~32767の値で得られるので512で割る(0~63)
-        var bar = "*".repeat(ntimes); // アスタリスク文字を、音量に応じて0~63回繰り返す
+        const vol = Math.abs(data.readInt16LE());  // 配列の先頭のサンプリングデータをひとつ読み込み、音量を得る
+        const ntimes = vol / 512; // 音量が0~32767の値で得られるので512で割る(0~63)
+        const bar = "*".repeat(ntimes); // アスタリスク文字を、音量に応じて0~63回繰り返す
         console.log("mic volume:", bar);
     });
 console.log('Listening, press Ctrl+C to stop.');
