212be92979b99c1a68fa6b0eabfef0aff3b1658a
kengo nakajima <kengo.nakajima@gmail.com>
2024-01-04 22:15:52 +0900

    refactor

--- app/mac.js
+++ app/mac.js
@@ -9,14 +9,6 @@ console.log("startMic ret:",r);
 r=addon.startSpeaker();
 console.log("startSpeaker ret:",r);
 
-let ary = new Int16Array(512);
-let t=0;
-for(let i=0;i<ary.length;i++) {
-  t=t+0.1;
-  ary[i]=Math.sin(t)*10000;
-}
-
-addon.pushSamplesForPlay(ary);
 
 setInterval(()=>{
   const samples=addon.getRecordedSamples();
