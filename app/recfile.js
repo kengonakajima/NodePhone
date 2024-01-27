ddc6c440ff535c4c2e29b4928e6bf5de3583b7b8
kengo nakajima <kengo.nakajima@gmail.com>
2024-01-27 22:09:47 +0900

    recfile

---
+++ app/recfile.js
@@ -0,0 +1,32 @@
+const {NativeAudio, getVolumeBar} = require('./util.js');
+const fs=require("fs");
+
+// NativeAudioの内部バッファを初期化する
+NativeAudio.initSampleBuffers(48000,48000); 
+// マイクを開始する
+NativeAudio.startMic(); 
+
+// 25ミリ秒に1回繰り返す
+setInterval(()=>{
+  // マイクからのサンプルを読み込む
+  const samples=NativeAudio.getRecordedSamples();
+  // サンプルがないときは何もせず、無名関数を終了
+  if(samples.length<=0) return;
+  // NativeAudioの内部バッファを破棄する
+  NativeAudio.discardRecordedSamples(samples.length); 
+
+  // samplesに含まれる最大音量を調べる。
+  let maxVol=0;
+  for(const sample of samples) {
+    if(sample>maxVol) maxVol=sample;
+  }
+  // 最大音量を表示する
+  const bar = getVolumeBar(maxVol);
+  console.log("mic volume:", bar, "len:",samples.length);
+
+  // 録音した音をファイルに保存する
+  const b=Buffer.from(samples.buffer);
+  fs.appendFileSync("recorded.lpcm16",b);
+},25);
+
+
