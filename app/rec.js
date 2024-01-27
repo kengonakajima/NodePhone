3fb14ed1ad036ae91e5405e5634f8a8ec55d473b
kengo nakajima <kengo.nakajima@gmail.com>
2024-01-27 19:38:04 +0900

    comment

--- app/rec.js
+++ app/rec.js
@@ -6,16 +6,18 @@ NativeAudio.startMic(); // マイクを開始する
 // 25ミリ秒に1回繰り返す
 setInterval(()=>{
   // マイクからのサンプルを読み込む
-  const samples=NativeAudio.getRecordedSamples(); 
-  if(samples.length<=0) return; // サンプルがないときは何もせず、無名関数を終了
-  NativeAudio.discardRecordedSamples(samples.length); // NativeAudioの内部バッファを破棄する
+  const samples=NativeAudio.getRecordedSamples();
+  // サンプルがないときは何もせず、無名関数を終了
+  if(samples.length<=0) return;
+  // NativeAudioの内部バッファを破棄する
+  NativeAudio.discardRecordedSamples(samples.length); 
 
-  // samplesに含まれる最大音量を調べる。  samplesの要素は -32768から32767の値を取る。
+  // samplesに含まれる最大音量を調べる。
   let maxVol=0;
   for(const sample of samples) {
     if(sample>maxVol) maxVol=sample;
   }
-  // 音量を表示する
+  // 最大音量を表示する
   const bar = getVolumeBar(maxVol);
   console.log("mic volume:", bar, new Date().getTime(), "len:",samples.length); 
 },25);
