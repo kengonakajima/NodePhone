03499871822689fa4001ea967aab163fc201b097
kengo nakajima <kengo.nakajima@gmail.com>
2024-01-27 22:20:56 +0900

    coment

--- app/nyquist.js
+++ app/nyquist.js
@@ -1,5 +1,6 @@
 const {NativeAudio} = require('./util.js');
-const freq=16000; // ナイキスト周波数の影響を体験するため、あえて低いサンプリング周波数を設定する
+// ナイキスト周波数の影響を体験するため、わざと低い周波数を指定する
+const freq=16000; 
 NativeAudio.initSampleBuffers(freq,freq);
 NativeAudio.startSpeaker();
 
@@ -12,7 +13,7 @@ function generate(sampleNum) {
   const hz=dt*freq/Math.PI/2.0; // 周波数を計算する
   const nyquist=freq/2; // 再生周波数が24000なので、その半分の12000Hzがナイキスト周波数
   const over=(hz>=nyquist);  // ナイキスト周波数を超えているか？
-  console.log("Freq:",freq,"NyquistHz:",nyquist,"CurrentHz:",Math.floor(hz),"Over-nyquist:",over); // 表示
+  console.log("Freq:",freq,"NyquistHz:",nyquist,"CurrentHz:",Math.floor(hz),"Over-nyquist:",over); 
   // 必要なサンプリングデータの数だけループさせる
   const outSamples=new Int16Array(sampleNum);
   for(var i=0;i<sampleNum;i++) { 
@@ -25,7 +26,9 @@ function generate(sampleNum) {
 }
 
 setInterval(()=>{
+  // 再生用バッファの残り量を調べる
   const used=NativeAudio.getPlayBufferUsed();
+  // 残り量が減ったら、次のサンプルを生成する
   if(used<4096) {
     const samples=generate(4096);
     NativeAudio.pushSamplesForPlay(samples);
