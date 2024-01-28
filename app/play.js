72f32c2fb52684f0de2fde32a277448b19b3dfb2
kengo nakajima <kengo.nakajima@gmail.com>
2024-01-28 11:30:54 +0900

    220hz

--- app/play.js
+++ app/play.js
@@ -1,7 +1,10 @@
 const {NativeAudio} = require('./util.js');
-NativeAudio.initSampleBuffers(48000,48000);
-NativeAudio.startSpeaker();
+const freq=48000; // サンプリング周波数
+NativeAudio.initSampleBuffers(freq,freq); //NativeAudioを初期化
+NativeAudio.startSpeaker(); // スピーカー起動
 
+const hz=220; // 生成する音の周波数
+const dt=Math.PI * 2 * hz / freq; // 1サンプルあたりの時間差
 let t=0;    // 音波を生成する際の時刻カウンター
 
 // サイン波を生成する。sampleNum: 生成するサンプル数
@@ -9,7 +12,7 @@ function generate(sampleNum) {
   // 必要なサンプリングデータの数だけループさせる
   const outSamples=new Int16Array(sampleNum);
   for(let i=0;i<sampleNum;i++) { 
-    t += Math.PI/16.0; // 1サンプルごとに時間を進める(2PI=3.14*2=6.28進めると1周期)
+    t += dt; // 1サンプルごとに時間を進める
     const y=Math.sin(t); // sinの値を求める
     const fsample=y*20000; // 振幅を掛ける    
     outSamples[i]=Math.floor(fsample); // 整数にする
@@ -17,9 +20,12 @@ function generate(sampleNum) {
   return outSamples;
 }
 
+// 25ミリ秒に1回繰り返す
 setInterval(()=>{
+  // 再生用バッファの残り量を調べる
   const used=NativeAudio.getPlayBufferUsed();
   if(used<4096) {
+    // 残り量が少ない場合は音を4096サンプルづつ生成する
     const samples=generate(4096);
     NativeAudio.pushSamplesForPlay(samples);
     console.log("pushed samples:",samples.length,new Date());
