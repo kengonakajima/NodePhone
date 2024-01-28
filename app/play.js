c72671b247fbfec4dcfa36ddd00cb17e6510dd4e
kengo nakajima <kengo.nakajima@gmail.com>
2024-01-28 12:08:13 +0900

    timing

--- app/play.js
+++ app/play.js
@@ -1,5 +1,5 @@
 const {NativeAudio} = require('./util.js');
-const freq=48000; // サンプリング周波数
+const freq=24000; // サンプリング周波数
 NativeAudio.initSampleBuffers(freq,freq); //NativeAudioを初期化
 NativeAudio.startSpeaker(); // スピーカー起動
 
@@ -12,14 +12,16 @@ setInterval(()=>{
   // 再生用バッファの残り量を調べる
   const used=NativeAudio.getPlayBufferUsed();
   if(used>=4096) return;
+  console.log("dt:",dt);
   // 残り量が少ない場合はサイン波を4096サンプルづつ生成する
   const n=4096;
   const samples=new Int16Array(n);
   for(let i=0;i<n;i++) { 
-    t += dt; // 1サンプルごとに時間を進める
     const y=Math.sin(t); // sinの値を求める
+    console.log(y);
     const sample=y*20000; // 振幅を掛ける    
     samples[i]=Math.floor(sample); // 整数にする
+    t += dt; // 1サンプル分の時間を進める
   }
   NativeAudio.pushSamplesForPlay(samples);
   console.log("pushed samples:",samples.length,new Date());
