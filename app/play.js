a8999da5e6d6ded7ac92923d40c21859e3c0cca7
kengo nakajima <kengo.nakajima@gmail.com>
2024-03-26 18:27:00 +0900

    clean

--- app/play.js
+++ app/play.js
@@ -7,15 +7,12 @@ const hz=220; // 生成する音の周波数
 const dt=Math.PI * 2 * hz / freq; // 1サンプルあたりの時間差
 let t=0;    // 音波を生成する際の時刻カウンター
 
-const st=new Date().getTime(); // 開始時刻
 // 25ミリ秒に1回繰り返す
 setInterval(()=>{
-  const curt=new Date().getTime(); // 現在時刻
-  const elt=curt-st; // 経過時間
   // 再生用バッファの残り量を調べる
   const used=PortAudio.getPlayBufferUsed();
   // 残り量が少ない場合はサイン波を4096サンプルづつ生成する
-  const n=4096*4;
+  const n=4096;
   if(used>=n) return;
   const samples=new Int16Array(n);
   for(let i=0;i<n;i++) { 
