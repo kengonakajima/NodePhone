fb7547ab461a5c25e29cfa3aa3e9fd66491f31c4
kengo nakajima <kengo.nakajima@gmail.com>
2024-07-05 16:19:04 +0900

    wip

--- app/origCancel.js
+++ app/origCancel.js
@@ -3,49 +3,250 @@
 
   
   */
-const {PortAudio} = require('./util.js');
-const freq=16000; 
-PortAudio.initSampleBuffers(freq,freq,256);
-PortAudio.startMic();
-PortAudio.startSpeaker();
-
 const {
+  spectrumBar,
+  energyBar,  
   getVolumeBar,
-}=require("./util.js");
+  findMaxSquare,
+  plotArrayToImage,
+  save_fs,
+  getMaxValue,
+  decimateFloat32Array,
+  loadLPCMFileSync,
+  to_f,
+  save_f
+} = require('./util.js');
+const freq=16000; 
 
 
 
-// 録音
-const g_recSamples=[]; // lpcm16。録音バッファ
-const g_refSamples=[]; // lpcm16 再生バッファ
+function maxSquarePeakIndex(ary) {
+  let maxEstimate=0;
+  let maxElement=ary[0];
+  for(let i=1;i<ary.length;i++) {
+    const v=ary[i]*ary[i];
+    if(v>maxElement) {
+      maxEstimate=i;
+      maxElement=v;
+    }
+  }
+  return maxEstimate;
+}
+// xStartIndex: xの開始位置
+// x2SumThreshold,smoothing: 設定値、定数
+// x: 参照信号
+// y: rec(16サンプル) low rate
+// h: ふぃるた係数の配列512個。
+let g_mfcore_cnt=0;
+function matchedFilterCore(xStartIndex,x2SumThreshold,smoothing,x,y,h) {
+  console.log("Matchedfiltercore called ", g_mfcore_cnt,":","xStartIndex:",xStartIndex,x.length,y.length,h.length);
+  g_mfcore_cnt++;
+
+  console.log("y",g_mfcore_cnt,":\n",y.join(","));
+  console.log("x",g_mfcore_cnt,":");
+  let xIndex=xStartIndex;
+  const xa=[];
+  for(let i=0;i<x.length;i++) {
+    xa.push(x[xIndex]);
+    xIndex = xIndex < (x.length - 1) ? xIndex + 1 : 0;
+  }
+  console.log(xa.join(","));
+  let maxH=0;
+  let maxHInd=-1;
+  console.log("h",g_mfcore_cnt,":\n",h.join(","));
+  for(let i=0;i<h.length;i++) {
+    const ah=Math.abs(h[i]);
+    if(ah>maxH) { maxH=ah; maxHInd=i; }
+  }
+  console.log("maxH:",maxH,"maxHInd:",maxHInd,"g_mfcore_cnt:",g_mfcore_cnt);
+
+  // 計算
+  let filterUpdated=false;
+  let errorSum=0;
+  for(let i=0;i<y.length;i++) {
+    let x2Sum=0;
+    let s=0;
+    let xIndex=xStartIndex;
+    for(let k=0;k<h.length;k++) {
+      x2Sum += x[xIndex] * x[xIndex];
+      s += h[k] * x[xIndex];
+      xIndex = xIndex < (x.length -1) ? xIndex + 1 : 0;
+    }
+    // 誤差信号を計算
+    const e = y[i] - s;
+    errorSum+=e*e;
+    const saturation = y[i] >= 32000 || y[i] <= -32000;
+    if(x2Sum > x2SumThreshold && !saturation) {
+      const alpha = smoothing * e / x2Sum;
+      let xIndex=xStartIndex;
+      for(let k=0;k<h.length;k++) {
+        h[k] += alpha * x[xIndex];
+        xIndex = xIndex < (x.length -1) ? xIndex + 1 : 0;
+      }
+      // 更新されたhを表示
+      console.log("looph'",i,g_mfcore_cnt,":",h.join(","));
+      let maxH=0;
+      let maxHInd=-1;
+      for(let i=0;i<h.length;i++) {
+        const ah=Math.abs(h[i]);
+        if(ah>maxH) { maxH=ah; maxHInd=i; }
+      }
+      console.log("maxH':",maxH,"maxHInd':",maxHInd);
+      filterUpdated=true;
+    }
+    xStartIndex = xStartIndex > 0 ? xStartIndex - 1 : x.length - 1; // 逆順なので一周したら末尾に戻す。
+  }
+  return {errorSum,filterUpdated};
+}
+
 
 function createOrigEC(freq) {
   const ec={};
-  ec.samples_per_frame= Math.floor(freq/100),
+  ec.samples_per_frame= Math.floor(freq/100);
+  // AEC3では[512]x5
+  const filterSize=512;
+  const filterNum=5;
+  ec.filters=new Array(5);
+  for(let i=0;i<filterNum;i++) ec.filters[i]=new Float32Array(filterSize); 
+  ec.xStartOfs=0; //ec.refのどの位置から読むか
 
+  ec.cnt=0;
+  
   ec.ref=[]; // 全部ため続ける(単純のため)
   ec.rec=[]; // processが終わったら削除する。
+  ec.recAccum=[]; // recをshiftせずにぜんぶためておく、デバッグ用
+  ec.out=[]; // 出力用
+
+  ec.renderBuffer={
+    buffer: new Float32Array(2448), // 2448=19*128, aec3. decimateされた信号を逆順に格納するリングバッファ
+    write: 2432, // 2448-16 aec3.
+    read: 2432, // 最初に書いたときに writeが2416になり、そのときに同じ値になるようにしておく。
+    copyBlock: function(sbReversed) {
+      if(sbReversed.length!=16) throw "invalid_size";
+      for(let i=0;i<16;i++) this.buffer[this.write+i]=sbReversed[i];
+      this.write-=16;  // 逆順リングバッファなので前に移動させていく。
+      if(this.write==-16) this.write=2432; // 一周する
+    },
+    dumpBuffer: function() {
+      console.log("renderBuffer dump: read:",this.read,"write:",this.write,"size:",this.buffer.length);
+      console.log(this.buffer.join(","));
+    },
+    incrementReadIndex: function() {
+      this.read-=16;
+      if(this.read==-16) this.read=2432; // 一周
+    }
+  };
+
+  
   // rec: i16ary
   ec.update_rec_frame = function(rec) {
-    for(const sample of rec) ec.rec.push(sample);    
+    if(rec.length%4>0) throw "invalid_rec_len";
+    for(const sample of rec) {
+      ec.rec.push(sample);
+      ec.recAccum.push(sample);
+    }
   }
   // ref: i16ary
   ec.update_ref_frame = function(ref) {
-    for(const sample of ref) ec.ref.push(sample);
+    if(ref.length%4>0) throw "invalid_ref_len";    
+    for(let i=0;i<ref.length;i++) ec.ref.push(ref[i]);
   }
+
+  ec.filters=[];
+  for(let i=0;i<5;i++) ec.filters[i]=new Float32Array(512); 
+
+
   // ms: 遅延の外部からの推定値?
   // i16out : 出力
   // ns: 1ならノイズキャンセルが有効
   ec.process = function(i16out) {
-    const version=0;
+    ec.cnt+=1;
+    
+    const version=1;
 
     if(version==0) {
-      // 何もせず入力を出力とする。これで綺麗にハウリングする事を確認。ノイズなどは入らない
+      // Version 1. 何もせず入力を出力とする。これで綺麗にハウリングする事を確認。ノイズなどは入らない
       if(ec.rec.length>=i16out.length) {
         for(let i=0;i<i16out.length;i++) {
           i16out[i]=ec.rec.shift();
         }
       }
+    } else if(version==1) {
+      // recに来ている信号で駆動する。つまりrecに1ブロックきたらrefも1ブロック進める。
+      const blockSize=64; // aec3の内部処理単位
+      const subBlockSize=16; // aec3 遅延推定用
+      const blockNum=Math.floor(ec.rec.length/blockSize);
+      for(let bi=0;bi<blockNum;bi++) {
+        const recBlock=new Float32Array(blockSize);
+        for(let i=0;i<blockSize;i++) recBlock[i]=ec.rec.shift();
+        const refBlock=new Float32Array(blockSize);
+        for(let i=0;i<blockSize;i++) refBlock[i]=ec.ref.shift();
+        console.log("bi:",bi,"recBlock:",recBlock,"refBlock:",refBlock);
+
+        //　以下、MatchedFilterを利用した遅延推定
+        
+        // ダウンサンプリングしたバッファを作成
+        const recLowBlock=decimateFloat32Array(recBlock,4);
+        const refLowBlockReversed=decimateFloat32Array(refBlock,4).reverse();
+        if(refLowBlockReversed.length!=subBlockSize) throw "invalid_size";
+        // renderBufferに1ブロックをコピーする
+        ec.renderBuffer.copyBlock(refLowBlockReversed);
+        ec.renderBuffer.dumpBuffer();
+        ec.renderBuffer.incrementReadIndex();
+
+        let errorSumAnchor = 0.0;
+        for(let k=0;k<recLowBlock.length;++k) errorSumAnchor += recLowBlock[k] * recLowBlock[k];
+        
+        // フィルタの計算
+        let winnerErrorSum=errorSumAnchor; // 最も誤差の小さいやつを探す。
+        let winnerIndex=-1;
+        let winnerLag=-1;
+        let alignmentShift=0;
+        for(let n=0;n<ec.filters.length;n++) {
+          const x2SumThreshold=512 * 150 * 150; // aec3. 512はフィルタサイズで　150は音量
+          // xStartIndexは2431から始まる
+          let xStartIndex = (ec.renderBuffer.read + alignmentShift + subBlockSize - 1 ) % ec.renderBuffer.buffer.length;
+          console.log("calling matchedFilterCore xStartIndex:",xStartIndex,"n:",n);
+          const smoothing=0.7;
+          const y=recLowBlock;
+          const coreOut=matchedFilterCore(xStartIndex,x2SumThreshold,smoothing,ec.renderBuffer.buffer,y,ec.filters[n]);
+
+          // フィルタのピークを監視して、遅延を推定する
+          const matchingFilterThreshold=0.2; //aec3
+          let lagEstimate = maxSquarePeakIndex(ec.filters[n]);
+          let reliable = lagEstimate > 2 && lagEstimate < (ec.filters[n].length - 10) &&
+              coreOut.errorSum < matchingFilterThreshold * errorSumAnchor;
+          const lag=lagEstimate + alignmentShift;
+          
+          console.log("lagEstimate:",lagEstimate,"reliable:",reliable,"lag:",lag);
+          if(reliable) {
+            plotArrayToImage([ec.filters[n]],1024,512,`plots/origcancel_reliable_${ec.cnt}_${n}.png`,1);
+          }
+          // ここまででフィルタのピークの位置を特定できている。勝者を選択する
+          if(coreOut.filterUpdated && reliable && coreOut.errorSum < winnerErrorSum) {
+            winnerLag=lag;
+            winnerIndex=n;
+            winnerErrorSum=coreOut.errorSum;
+            console.log("winner updated. n:",n,"lag:",lag);
+          }
+
+          
+          
+          alignmentShift+=24*16; // aec3
+        }
+        if(winnerIndex!=-1) {
+          const reportedLagEstimate=winnerLag;
+          console.log("winnerLag:",winnerLag);
+        }
+        for(let i=0;i<blockSize;i++) ec.out.push(recBlock[i]);
+      }
+
+      // 出力
+      if(ec.out.length>=i16out.length) {
+        for(let i=0;i<i16out.length;i++) {
+          i16out[i]=ec.out.shift();
+        }
+      }      
     }
   }
   ec.get_metrics_echo_return_loss_enhancement = function() {
@@ -56,58 +257,48 @@ function createOrigEC(freq) {
 
 const ec=createOrigEC(freq);
 
-setInterval(()=>{
-  let recMax=0, playMax=0;
-  let enh=0;
-  
-  // マイクからのサンプルを読み込む
-  const samples=PortAudio.getRecordedSamples(); 
-  if(samples.length<=0) return; // サンプルがないときは何もせず、無名関数を終了
-  PortAudio.discardRecordedSamples(samples.length); // PortAudioの内部バッファを破棄する
 
-  // samplesに含まれる最大音量を調べる。  samplesの要素は -32768から32767の値を取る。
-  let maxVol=0;
-  for(const sample of samples) {
-    if(sample>recMax) recMax=sample;
-    g_recSamples.push(sample); // 録音バッファに記録
-  }
+const played=loadLPCMFileSync("counting48k.lpcm").slice(0,50000);  // 元のデータ。これが再生用データ
+const recorded=loadLPCMFileSync("playRecCounting48k.lpcm16").slice(0,50000);  // counting48k.lpcmをplayrec.jsで録音した48KHzのデータ
 
-  // 録音バッファに音が来ていたらエコーキャンセラを呼び出す
-  if(g_recSamples.length>=ec.samples_per_frame ) {
-    let frameNum=Math.floor(g_recSamples.length/ec.samples_per_frame);
-    if(frameNum>10) frameNum=10;
-    for(let j=0;j<frameNum;j++) {      
-      const rec=new Int16Array(ec.samples_per_frame);
-      for(let i=0;i<ec.samples_per_frame;i++) {
-        rec[i]=g_recSamples.shift();
-      }
-      ec.update_rec_frame(rec); // 録音サンプルをAECに渡す
-      const ref=new Int16Array(ec.samples_per_frame);
-      for(let i=0;i<ec.samples_per_frame;i++) {
-        ref[i]=g_refSamples.shift();
-      }
-      ec.update_ref_frame(ref); // 前回記録した参照バッファをAECに渡す
-      const processed=new Int16Array(ec.samples_per_frame);
-      ec.process(processed); // AECの実際の処理を実行する
-      playMax=0;
-      const play=new Int16Array(ec.samples_per_frame);
-      for(let i=0;i<ec.samples_per_frame;i++) {
-        const sample=processed[i];
-        g_refSamples.push(sample); // AEC処理された音を参照バッファに送る
-        play[i]=sample;         // 同じ音を再生バッファに送る
-        if(sample>playMax) playMax=sample;
-      }
-      PortAudio.pushSamplesForPlay(play);  // スピーカーに送る     
-    }
-    enh=ec.get_metrics_echo_return_loss_enhancement(); // 統計情報を取得
+for(let i=0;i<160;i++) recorded[i]=0; // aec3では最初のちゃんくについてなぜかすべて0になっている。
+
+const chunkSize=160;
+
+const finalOut=new Float32Array(recorded.length*2);
+
+console.log("played:",played.length,"recorded:",recorded.length,"chunkSize:",chunkSize);
+
+for(let l=0;;l++) {
+  const startIndex=l*chunkSize;
+  if(startIndex>played.length)break;
+  const recChunk=new Int16Array(chunkSize);
+  for(let i=0;i<chunkSize;i++) recChunk[i]=recorded[startIndex+i]||0;
+  const refChunk=new Int16Array(chunkSize);
+  for(let i=0;i<chunkSize;i++) refChunk[i]=played[startIndex+i]||0;
+
+  ec.update_rec_frame(recChunk); // 録音サンプルをAECに渡す
+  ec.update_ref_frame(refChunk); // 前回記録した参照バッファをAECに渡す
+  const processed=new Int16Array(chunkSize);
+  console.log("Starting chunk process:",l);
+  ec.process(80,processed,0); // AECの実際の処理を実行する
+
+  for(let i=0;i<processed.length;i++) {
+    finalOut[startIndex+i]=to_f(processed[i]);
   }
+  const enh=ec.get_metrics_echo_return_loss_enhancement(); // 統計情報を取得
 
   // デバッグ表示
-//  process.stdout.write('\033c');  
-  console.log("rec:",getVolumeBar(recMax));
-  console.log("play:",getVolumeBar(playMax));
-  console.log("playMax:",playMax);
-  console.log("recSamples:",g_recSamples.length);
-  console.log("refSamples:",g_refSamples.length);  
-  console.log("Enhance:",enh);
-},20);
+  console.log("chunk:",l,
+              "rec:",getMaxValue(recChunk),
+              "ref:",getMaxValue(refChunk),
+              "out:",getMaxValue(processed),
+              "enh:",enh
+             );
+}
+
+console.log("done");
+save_f(finalOut,"origStatic.lpcm16");
+
+
+
