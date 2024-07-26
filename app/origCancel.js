302e38cd3153d9c9f82ee5afcadb5e0302ae89c6
kengo nakajima <kengo.nakajima@gmail.com>
2024-07-26 11:42:16 +0900

    lowbuffer

--- app/origCancel.js
+++ app/origCancel.js
@@ -8,6 +8,7 @@ const {
   energyBar,  
   getVolumeBar,
   findMaxSquare,
+  findMax,
   plotArrayToImage,
   save_fs,
   getMaxValue,
@@ -99,7 +100,18 @@ function matchedFilterCore(xStartIndex,x2SumThreshold,smoothing,x,y,h) {
   return {errorSum,filterUpdated};
 }
 
-
+function createSampleBuffer(n,readInit,writeInit) {
+  const out={
+    buf: new Float32Array(n),
+    read: readInit,
+    write: writeInit,
+    dump: function() {
+      console.log("SampleBuffer dump: read:",this.read,"write:",this.write,"size:",this.buf.length);
+      console.log(this.buf.join(","));
+    },
+  };
+  return out;
+}
 function createOrigEC(freq) {
   const ec={};
   ec.samples_per_frame= Math.floor(freq/100);
@@ -118,22 +130,31 @@ function createOrigEC(freq) {
   ec.out=[]; // 出力用
 
   ec.renderBuffer={
-    buffer: new Float32Array(2448), // 2448=19*128, aec3. decimateされた信号を逆順に格納するリングバッファ
-    write: 2432, // 2448-16 aec3.
-    read: 2432, // 最初に書いたときに writeが2416になり、そのときに同じ値になるようにしておく。
-    copyBlock: function(sbReversed) {
+    // 2448=19*128+16, aec3. decimateされた信号を逆順に格納するリングバッファ. MatchedFilter用
+    // write: 2448-16 aec3.
+    // read: 最初に書いたときに writeが2416になり、そのときに同じ値になるようにしておく。    
+    bufferLowReversed: createSampleBuffer(2448,2432,2432),
+    // decimateしない元の信号用. Adaptive FIR Filter用。 正順
+    bufferHigh: createSampleBuffer(2448*4,2432*4,2432*4),
+    copyBlockLowReversed: function(sbReversed) {
       if(sbReversed.length!=16) throw "invalid_size";
-      for(let i=0;i<16;i++) this.buffer[this.write+i]=sbReversed[i];
-      this.write-=16;  // 逆順リングバッファなので前に移動させていく。
-      if(this.write==-16) this.write=2432; // 一周する
+      for(let i=0;i<16;i++) this.bufferLowReversed.buf[this.bufferLowReversed.write+i]=sbReversed[i];
+      this.bufferLowReversed.write-=16;  // 逆順リングバッファなので前に移動させていく。
+      if(this.bufferLowReversed.write==-16) this.bufferLowReversed.write=2432; // 一周する
     },
-    dumpBuffer: function() {
-      console.log("renderBuffer dump: read:",this.read,"write:",this.write,"size:",this.buffer.length);
-      console.log(this.buffer.join(","));
+    incrementReadIndexLowReversed: function() {
+      this.bufferLowReversed.read-=16;
+      if(this.bufferLowReversed.read==-16) this.bufferLowReversed.read=2432; // 一周
     },
-    incrementReadIndex: function() {
-      this.read-=16;
-      if(this.read==-16) this.read=2432; // 一周
+    copyBlockHigh: function(sb) {
+      if(sb.length!=64) throw "invalid_size";
+      for(let i=0;i<64;i++) this.bufferHigh.buf[this.bufferHigh.write+i]=sb[i];
+      this.bufferHigh.write+=64;
+      if(this.bufferHigh.write==this.bufferHigh.buf.length) this.bufferHigh.write=0;
+    },
+    incrementReadIndexHigh: function() {
+      this.bufferHigh.read+=64;
+      if(this.bufferHigh.read==this.bufferHigh.buf.length) this.bufferHigh.read=0; // 1周
     }
   };
 
@@ -155,7 +176,19 @@ function createOrigEC(freq) {
   ec.filters=[];
   for(let i=0;i<5;i++) ec.filters[i]=new Float32Array(512); 
 
+  ec.histogramData=new Int16Array(250); // aec3 MatchedFilterの推定値の単純な履歴
+  ec.histogram=new Int16Array(19*128+1); // aec3 renderBufferのサイズが 19*128+16。対応できる最大の遅延。
+  ec.histogramDataIndex=0;
+  
 
+  ec.debugLowRef=[];
+  ec.debugLowRec=[];
+
+  ec.maxCoreOutErrorSum=0;
+  ec.totalCoreOutErrorSum=0;
+
+  ec.totalLatencyBlocks=0;
+  
   // ms: 遅延の外部からの推定値?
   // i16out : 出力
   // ns: 1ならノイズキャンセルが有効
@@ -190,9 +223,14 @@ function createOrigEC(freq) {
         const refLowBlockReversed=decimateFloat32Array(refBlock,4).reverse();
         if(refLowBlockReversed.length!=subBlockSize) throw "invalid_size";
         // renderBufferに1ブロックをコピーする
-        ec.renderBuffer.copyBlock(refLowBlockReversed);
-        ec.renderBuffer.dumpBuffer();
-        ec.renderBuffer.incrementReadIndex();
+        ec.renderBuffer.copyBlockLowReversed(refLowBlockReversed);
+        //ec.renderBuffer.dumpBuffer();
+        ec.renderBuffer.incrementReadIndexLowReversed();
+        // デバッグ用の記録
+        for(let i=0;i<subBlockSize;i++) {
+          ec.debugLowRec.push(recLowBlock[i]);
+          ec.debugLowRef.push(refLowBlockReversed[subBlockSize-1-i]);
+        }
 
         let errorSumAnchor = 0.0;
         for(let k=0;k<recLowBlock.length;++k) errorSumAnchor += recLowBlock[k] * recLowBlock[k];
@@ -205,11 +243,11 @@ function createOrigEC(freq) {
         for(let n=0;n<ec.filters.length;n++) {
           const x2SumThreshold=512 * 150 * 150; // aec3. 512はフィルタサイズで　150は音量
           // xStartIndexは2431から始まる
-          let xStartIndex = (ec.renderBuffer.read + alignmentShift + subBlockSize - 1 ) % ec.renderBuffer.buffer.length;
+          let xStartIndex = (ec.renderBuffer.bufferLowReversed.read + alignmentShift + subBlockSize - 1 ) % ec.renderBuffer.bufferLowReversed.buf.length;
           console.log("calling matchedFilterCore xStartIndex:",xStartIndex,"n:",n);
           const smoothing=0.7;
           const y=recLowBlock;
-          const coreOut=matchedFilterCore(xStartIndex,x2SumThreshold,smoothing,ec.renderBuffer.buffer,y,ec.filters[n]);
+          const coreOut=matchedFilterCore(xStartIndex,x2SumThreshold,smoothing,ec.renderBuffer.bufferLowReversed.buf,y,ec.filters[n]);
 
           // フィルタのピークを監視して、遅延を推定する
           const matchingFilterThreshold=0.2; //aec3
@@ -227,19 +265,44 @@ function createOrigEC(freq) {
             winnerLag=lag;
             winnerIndex=n;
             winnerErrorSum=coreOut.errorSum;
-            console.log("winner updated. n:",n,"lag:",lag);
+            if(coreOut.errorSum>ec.maxCoreOutErrorSum) ec.maxCoreOutErrorSum=coreOut.errorSum;
+            ec.totalCoreOutErrorSum+=coreOut.errorSum;
+            console.log("winner updated. n:",n,"lag:",lag,"errorSum:",coreOut.errorSum,"maxCoreOutErrorSum:",ec.maxCoreOutErrorSum,"totalCoreOutErrorSum:",ec.totalCoreOutErrorSum,"lagEstimate:",lagEstimate,"alignmentShift:",alignmentShift);
           }
-
-          
-          
           alignmentShift+=24*16; // aec3
         }
+        // MatchedFilterがだいたいの遅れを検出した。
         if(winnerIndex!=-1) {
           const reportedLagEstimate=winnerLag;
-          console.log("winnerLag:",winnerLag);
+          console.log("reportedLagEstimate:",reportedLagEstimate,"histogramDataIndex:",ec.histogramDataIndex);
+          // この結果をhistogramで安定化する HighestPeakAggregator::Aggregate相当
+          ec.histogram[ec.histogramData[ec.histogramDataIndex]]--;
+          ec.histogramData[ec.histogramDataIndex]=reportedLagEstimate;
+          ec.histogram[ec.histogramData[ec.histogramDataIndex]]++;
+          ec.histogramDataIndex = (ec.histogramDataIndex+1) % ec.histogramData.length;
+          const candidate=findMax(ec.histogram);
+          console.log("candidate:",candidate);
+          console.log("histogram:",ec.histogram.join(","));
+          console.log("histogram_data:",ec.histogramData.join(","));
+          // 4KHzサンプルでの遅延がわかったので、次にブロック数にする (render_delay_buffer)
+          
+          const renderBufferLatencySamples = (ec.renderBuffer.bufferLowReversed.buf.length + ec.renderBuffer.bufferLowReversed.read - ec.renderBuffer.bufferLowReversed.write) % ec.renderBuffer.bufferLowReversed.buf.length;
+          const renderBufferLatencyBlocks = renderBufferLatencySamples / subBlockSize;
+          const candidateLatencyBlocks = Math.floor(candidate.index/subBlockSize); // TODO:ゆらぐこともあるよね
+          ec.totalLatencyBlocks = candidateLatencyBlocks+renderBufferLatencyBlocks;
+          console.log("renderBufferLatencySamples:",renderBufferLatencySamples);
+          console.log("renderBufferLatencyBlocks:",renderBufferLatencyBlocks);
+          console.log("candidateLatencyBlocks:",candidateLatencyBlocks);
+          console.log("ec.totalLatencyBlocks:",ec.totalLatencyBlocks,"subBlockSize:",subBlockSize);
         }
+        if(ec.totalLatencyBlocks) {
+          // renderBufferは逆順
+          console.log("RRRR:",ec.renderBuffer.bufferLowReversed.read);
+        }
+
+        
         for(let i=0;i<blockSize;i++) ec.out.push(recBlock[i]);
-      }
+      } // version 1
 
       // 出力
       if(ec.out.length>=i16out.length) {
@@ -300,5 +363,6 @@ for(let l=0;;l++) {
 console.log("done");
 save_f(finalOut,"origStatic.lpcm16");
 
-
+save_fs(ec.debugLowRef,"debugLowRef.lpcm16");
+save_fs(ec.debugLowRec,"debugLowRec.lpcm16");
 
